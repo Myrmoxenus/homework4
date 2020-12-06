dcfb8d6d91e495f184b61ba4ff3b9580383b38a8
Myrmoxenus <scopocira@gmail.com>
2020-12-05 23:02:06 -0600

    Majority of homework functionality present, will revisit soon

---
+++ script.js
@@ -0,0 +1,151 @@
+
+
+// Variable that stores which answer is currently selected by the user, defaults with first answer selected
+var selectedAnswer = "0"
+
+//Series of event listeners for the radio buttons in the HTML file that store the letter corresponding to the user's selected in the selected answer variable.
+var aButton = document.getElementById("aButton")
+aButton.addEventListener("click", function(){ 
+    selectedAnswer = "0"
+})  
+var bButton = document.getElementById("bButton")
+bButton.addEventListener("click", function(){ 
+    selectedAnswer = "1"
+})  
+var cButton = document.getElementById("cButton")
+cButton.addEventListener("click", function(){ 
+    selectedAnswer = "2"
+})  
+var dButton = document.getElementById("dButton")
+dButton.addEventListener("click", function(){ 
+    selectedAnswer = "3"
+})  
+
+// Series of variables for as keys for manipulating inner text of question answers
+var questionDisplayText = document.getElementById("questionText")
+var answerDisplayTextA = document.getElementById("answerA")
+var answerDisplayTextB = document.getElementById("answerB")
+var answerDisplayTextC = document.getElementById("answerC")
+var answerDisplayTextD = document.getElementById("answerD")
+
+// An array to store all possible answers to current question
+var answerArray = []
+
+// Takes a question's index then places all answers, wrong and correct into an array
+function grabAnswers(index) {
+    answerArray = Object.values(questionsObject[index].wrongAnswers)
+    answerArray.push(questionsObject[index].correctAnswer)
+}
+
+//Generates a random integer from 0 up to the value of randomMax.
+function randomUpTo(randomMax) {
+    return ((Math.floor(Math.random() * (randomMax + 1))))
+}
+
+//An array to store all possible answers to question, shuffled
+var shuffledArray = []
+
+//Removes a random member of an array and places it into shuffledArray
+function shuffle(array) {
+var randomIndex = randomUpTo(answerArray.length - 1)
+shuffledArray.push(answerArray[randomIndex])
+answerArray.splice(randomIndex, 1)
+}
+
+//Shuffles all values in answerArray into shuffledArray, empties answerArray
+function shuffleAll(array) {
+    for (i=1; i=array.length; i++) {
+    shuffle(array)
+    }
+}
+
+//Terminates quiz and sends user to scorepage
+function terminate(){
+    //Stores user score, prompts user to supply name
+    localStorage.setItem("playerScore"+localStorage.length, score)
+    //Redirects to scorecard page
+    window.location.href = "scorecard.html"    
+}
+
+//Renders new randomly selected question with shuffled answers to screen
+function renderRandomQuestion(){
+    // Resets shuffled array
+    shuffledArray = []
+    //If there are no remaining novel questions, terminates and sends user to scorecard
+    if(questionsObject.length == 0) {
+        terminate()
+    }
+    // Selects a question at random from questionsObject
+    var randomIndex = randomUpTo(questionsObject.length - 1)
+    //Grabs all possible answers and shuffles their order
+    grabAnswers(randomIndex)
+    shuffleAll(answerArray)
+    //Writes question possible answers into the correct spot in the document
+    questionDisplayText.innerHTML = questionsObject[randomIndex].question
+    answerDisplayTextA.innerHTML = shuffledArray[0]
+    answerDisplayTextB.innerHTML = shuffledArray[1]
+    answerDisplayTextC.innerHTML = shuffledArray[2]
+    answerDisplayTextD.innerHTML = shuffledArray[3]
+    // Stores the correct answer in a varialbe
+    currentCorrectAnswer = questionsObject[randomIndex].correctAnswer
+    // Removes question from questionsObject to ensure questions are novel
+    questionsObject.splice(randomIndex, 1)
+}
+
+//Checks if selected answer is correct
+function userCorrect(){
+    return currentCorrectAnswer == shuffledArray[selectedAnswer]
+}
+
+//Stores player score as a variable, writes it to document. Score defaults to 0
+displayedScore = document.getElementById("playerScore")
+score = 0
+playerScore.innerHTML = "Player Score: " + score
+
+
+//Event listener for when the user has clicked the submit button
+var submitButton = document.getElementById("submitButton")
+submitButton.addEventListener("click", function(){ 
+   //Checks if user is correct and adds score if so
+   if (userCorrect()){
+    score++
+   }
+   //Renders a new question and user's updated score to the document
+   playerScore.innerHTML = "Player Score: " + score
+   renderRandomQuestion()
+}
+)
+
+//Displays how much time user has left on timer
+timeLeft = document.getElementById("timer")
+var minutesRemaining = 0
+var secondsRemaining = 0
+timeLeft.innerHTML = "Time Remaining: " + minutesRemaining + " min " + secondsRemaining + " sec"
+
+//Timer takes minutes and seconds as arguments
+function timer(minutes, seconds){
+minutesRemaining = minutes
+secondsRemaining = seconds
+setInterval(timeCount, 1000)}
+
+//Second counter, draws from minutes when seconds reaches 0
+function timeCount(){
+
+    //Terminates and sends user to scorecard when clock reaches 0 minutes and 0 seconds
+    if(secondsRemaining == 0 && minutesRemaining == 0){
+    terminate()
+    }
+
+    if(secondsRemaining == 0){
+    secondsRemaining = 60
+    minutesRemaining = minutesRemaining - 1
+    }
+
+  secondsRemaining = secondsRemaining - 1
+  timeLeft.innerHTML = "Time Remaining: " + minutesRemaining + " min " + secondsRemaining + " sec"
+}
+
+
+//Renders question on first open and sets timer
+renderRandomQuestion()
+timer(0, 16)
